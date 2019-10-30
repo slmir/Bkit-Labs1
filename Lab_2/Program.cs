c5c34e3b9dae29e68a889ffd3e07be9e4f54b65f
slmir <mirsonov43153@gmail.com>
2019-10-30 12:23:48 +0300

    Latest

--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -6,56 +6,19 @@ using System.Threading.Tasks;
 
 namespace Lab_2
 {
-    /// <summary>
-    /// Version 1.0.6
-    /// 
-    /// </summary>
     class ProgramLR2
     {
         static void Main(string[] args)
         {
             Console.Title = "Мирсонов Вячеслав РТ5-31Б";
-            string str = "нет";
-            do//цикл выполняется, пока пользователь не завершит выполнение программы
-            {
-                Console.WriteLine("Выберете одну из предлложенных фигур, площадь которой вы хотите вычислить: \n");
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("1. Прямоугольник \n2. Квадрат \n3. Круг");//вывод возможных вариантов
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Console.Write("\nВведите номер выбранного варианта: ");
-                char v = Convert.ToChar(Console.ReadLine());//ожидание ввода выбранного варианта
-                Console.WriteLine("\nВведите следующие значения для выбранной геометрической фигуры");
-                switch (v)//для обозначения вводимого коэффициента
-                {
-                    case '1'://если выбран Прямоугольник
-                        Console.Write("Введите высоту прямоугольника: ");
-                        double v_rect = Convert.ToDouble(Console.ReadLine());//высота прямоугольника
-                        Console.Write("Введите ширину прямоугольника: ");
-                        double w_rect = Convert.ToDouble(Console.ReadLine());//ширина прямоугольника
-                        Rectangle obj1 = new Rectangle(v_rect, w_rect);//создание объекта класса Прямокгольник и передача введеных ранее параметров в конструктор
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        obj1.Print();//вызов метода печати данных об объекте (выбранной геометрической фигуре)
-                        break;
-                    case '2':
-                        Console.Write("Введите длину стороны квадрата: ");
-                        double cz = Convert.ToDouble(Console.ReadLine());//длина стороны квадрата
-                        Square obj2 = new Square(cz);//создание объекта класса Квадрат и передача введеного ранее параметра в конструктор
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        obj2.Print();//вызов метода печати данных об объекте (выбранной геометрической фигуре)
-                        break;
-                    case '3':
-                        Console.Write("Введите радиус круга: ");
-                        double rd = Convert.ToDouble(Console.ReadLine());//радиус круга
-                        Circle obj3 = new Circle(rd);//создание объекта класса Круг и передача введеного ранее параметра в конструктор
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        obj3.Print();//вызов метода печати данных об объекте (выбранной геометрической фигуре)
-                        break;
-                }
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Console.Write("\nДля продолжения работы введите 'да', для прекращения 'нет': ");
-                str = Convert.ToString(Console.ReadLine());//строка, вводимая пользователем как ответ на желание продолжить либо прекратить выполнение программы
-                Console.Write("\n");
-            } while (str == "да");//пока пользователь желает продолжать выполнение программы
+            Rectangle obj1 = new Rectangle(10,20);//создание объекта класса Прямокгольник и передача введеных ранее параметров в конструктор
+            Square obj2 = new Square(15);//создание объекта класса Квадрат и передача введеного ранее параметра в конструктор
+            Circle obj3 = new Circle(20);//создание объекта класса Круг и передача введеного ранее параметра в конструктор
+            Console.ForegroundColor = ConsoleColor.Green;
+            obj1.Print();//вызов метода печати данных об объекте (выбранной геометрической фигуре)
+            obj2.Print();//вызов метода печати данных об объекте (выбранной геометрической фигуре)           
+            obj3.Print();//вызов метода печати данных об объекте (выбранной геометрической фигуре)           
+            Console.ForegroundColor = ConsoleColor.Gray;    
             Console.ReadLine();
         }
     }
